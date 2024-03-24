292bc1a55e3c54a6ea9ae1ba10df7607a6201f83
afterthemagic <parannou7@gmail.com>
2024-03-24 17:20:57 +0800

    Updated style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,4 +5,19 @@ export default {
 		extend: {},
 	},
 	plugins: [],
-}
+},
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	theme: {
+	  colors: {
+		'accent-1': '#DAAC39',
+		'accent-2': '#DABB5F',
+		'accent-3': '#DACA9A',
+		'main-1': '#889639',
+		'main-2': '#9EAD41',
+		'background-1': '#121000',
+		'background-2': '#161700',
+	  }
+	}
+  }
